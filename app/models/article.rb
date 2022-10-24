ae99821f93f6bd6294cf7816351d64aae3d1e03c
Stensis <irenenjuguna98@gmail.com>
2022-10-24 14:42:11 +0300

    added associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ApplicationRecord
+    belongs_to :author
+    belongs_to :category
+    has_many :bookmarks
+    has_many :comments
+    has_many :likes
+    has_many :users, through: :likes
 end
