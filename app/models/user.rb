9fe717d4058f90b7f02da4a6d2f72f2a56694ee9
Daryl Rogony <darylrogony@gmail.com>
2022-10-25 15:07:10 +0300

    Fit: update user actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_many :categories
     has_many :articles
+    has_many :bookmarks
 
     validates :password, presence: true
-    validates :email, uniqueness: true
+    # validates :email, uniqueness: true
     validates :password_digest, confirmation: true
 end
